30194cb969b1177903780a81dcf116af529dbfa7
donblaseball <donblaseball@gmail.com>
2020-08-15 22:06:38 -0400

    Updates

---
+++ precache-manifest.9619831d65ab4351c98cc6b439698b4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0090dd37cfd81ae77497fe100f30255",
+    "url": "/blaseball-stats/index.html"
+  },
+  {
+    "revision": "38f01b2ba904c187df91",
+    "url": "/blaseball-stats/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "9763d853a4964ae86f6e",
+    "url": "/blaseball-stats/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "38f01b2ba904c187df91",
+    "url": "/blaseball-stats/static/js/2.73e291a8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/blaseball-stats/static/js/2.73e291a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9763d853a4964ae86f6e",
+    "url": "/blaseball-stats/static/js/main.35964d22.chunk.js"
+  },
+  {
+    "revision": "4380247d9f54bd2a0f38",
+    "url": "/blaseball-stats/static/js/runtime-main.d38d4ed9.js"
+  }
+]);
